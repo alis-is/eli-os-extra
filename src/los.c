0b0ab2c2c6c311eba5c52b674049ad5f65268c2a
v <cryi@tutanota.com>
2024-09-13 22:22:52 +0200

    usleep -> nanosleep

--- src/los.c
+++ src/los.c
@@ -7,13 +7,13 @@
 #ifdef _WIN32
 #include <windows.h>
 
-#define _lsleep          Sleep
+#define _lsleep Sleep
 #define SLEEP_MULTIPLIER 1e3
 #else
 #include <unistd.h>
 
-#define _lsleep          usleep
-#define SLEEP_MULTIPLIER 1e6
+#define _lsleep nanosleep
+#define SLEEP_MULTIPLIER 1e9
 #endif
 
 /*
@@ -26,24 +26,24 @@
 seconds --
 interval units --
 */
-static int
-eli_sleep(lua_State* L) {
-    lua_Number interval = luaL_checknumber(L, 1);
-    lua_Number units = luaL_optnumber(L, 2, 1);
-    _lsleep(SLEEP_MULTIPLIER * interval / units);
-    return 0;
+static int eli_sleep(lua_State *L)
+{
+	lua_Number interval = luaL_checknumber(L, 1);
+	lua_Number units = luaL_optnumber(L, 2, 1);
+	_lsleep(SLEEP_MULTIPLIER * interval / units);
+	return 0;
 }
 
 static const struct luaL_Reg eliOsExtra[] = {
-    {"sleep", eli_sleep},
-    {"chdir", eli_chdir},
-    {"cwd", eli_cwd},
-    {NULL, NULL},
+	{ "sleep", eli_sleep },
+	{ "chdir", eli_chdir },
+	{ "cwd", eli_cwd },
+	{ NULL, NULL },
 };
 
-int
-luaopen_eli_os_extra(lua_State* L) {
-    lua_newtable(L);
-    luaL_setfuncs(L, eliOsExtra, 0);
-    return 1;
+int luaopen_eli_os_extra(lua_State *L)
+{
+	lua_newtable(L);
+	luaL_setfuncs(L, eliOsExtra, 0);
+	return 1;
 }
